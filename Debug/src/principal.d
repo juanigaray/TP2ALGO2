6534fb4d7d8b8bcbef86700c7e9c942e055140df
Juan Garay <juangaray93@gmail.com>
2017-10-20 14:08:25 -0300

    Cambio la estructura de la carpeta

---
+++ Debug/src/principal.d
@@ -0,0 +1,14 @@
+src/principal.o: ../src/principal.cpp ../src/Dibujante.h \
+ ../src/EasyBMP/EasyBMP.h ../src/EasyBMP/EasyBMP_DataStructures.h \
+ ../src/EasyBMP/EasyBMP_BMP.h \
+ ../src/EasyBMP/EasyBMP_VariousBMPutilities.h
+
+../src/Dibujante.h:
+
+../src/EasyBMP/EasyBMP.h:
+
+../src/EasyBMP/EasyBMP_DataStructures.h:
+
+../src/EasyBMP/EasyBMP_BMP.h:
+
+../src/EasyBMP/EasyBMP_VariousBMPutilities.h:
