8729b4ea308034740fc249739ad60036a36f8995
JuanGaray93 <31663056+JuanGaray93@users.noreply.github.com>
2017-10-21 16:30:48 -0300

    Sumo comentarios

--- src/Arbitro.h
+++ src/Arbitro.h
@@ -17,17 +17,54 @@ class Arbitro{
 	uint columnaMaxima;
 
   public:
-
-	Arbitro(uint dificultad, uint numeroDeJugadores, uint filas, uint columnas);
-
-	uint devolverColumnaDeJugada();
-	uint devolverFilaDeJugada();
-	std::string devolverTipoDeJugada();
-	uint devolverTurno();
-	uint devolverPuntaje();
-	bool terminoElJuego();
-	void declararTurno();
+	/* Pre: Se le pasan numeros naturales.
+	 * Post: Crea un arbitro listo para pasarle jugadas
+	 */
+	Arbitro(uint dificultad, uint numeroDeJugadores, uint filas, uint columnas, std::string* listaDeJugadores);
+	
+	/*
+	 * Post: Le toma la jugada al usuario. 
+	 * Decide el resultado de la jugada.
+	 */
 	void tomarJugada();
+	
+	/*
+	 * Post: Devuelve en que columna se realizo la ultima jugada tomada.
+	 */
+	uint devolverColumnaDeJugada();
+	
+	/*
+	 * Post: Devuelve en que columna se realizo la ultima jugada tomada.
+	 */
+	uint devolverFilaDeJugada();
+	
+	/*
+	 * Post: Devuelve string con el tipo de jugada que se realizo.
+	 */
+	std::string devolverTipoDeJugada();
+	
+	/*
+	 * Post: Devuelve el numero de jugador que realizo la ultima jugada.
+	 */
+	uint devolverTurno();
+	
+	void declararTurno();
+	
+	/*
+	 * Post: Devuelve el puntaje total del jugador que jugo ultimo.
+	 *	 Devuelve -1 si no se modifica.
+	 */
+	int devolverPuntaje();
+	
+	/*
+	 * Post: 
+	 */
+	bool terminoElJuego();
+	
+
+	
+
+	
 };
 
 
