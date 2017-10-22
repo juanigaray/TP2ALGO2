16732ac68156481d43fbe19a44d676a691a6ff82
JuanGaray93 <31663056+JuanGaray93@users.noreply.github.com>
2017-10-22 19:04:14 -0300

    Create Arbitro.cpp

---
+++ src/Arbitro.cpp
@@ -0,0 +1,53 @@
+
+/*
+ * Arbitro.cpp
+ *
+ *  Created on: Oct 22, 2017
+ *      Author: juan
+ */
+ 
+ #include "Arbitro.h"
+ 
+Arbitro::Arbitro(uint dificultadPedida, uint numeroDeJugadores, uint filas, uint columnas, std::string* listaDeNombresDeJugadores){
+
+	dificultad = dificultadPedida;
+	cantJugadores = numeroDeJugadores;
+	filaMaxima = filas;
+	columnaMaxima = columnas;
+	listaDeJugadores.inicializar(listaDeNombresDeJugadores);
+	listaDeBombas.inicializar(dificultad);
+	listaDeBanderas.inicializar();
+}
+
+void Arbitro::tomarJugada(){
+	//modularizar
+ 
+}
+
+uint Arbitro::devolverColumnaDeJugada(){
+
+}
+
+uint Arbitro::devolverFilaDeJugada(){
+
+}
+	
+std::string Arbitro::devolverTipoDeJugada(){
+
+}
+	
+uint Arbitro::devolverTurno(){
+
+}
+	
+void Arbitro::declararTurno(){
+
+}
+	
+int Arbitro::devolverPuntaje(){
+
+}
+
+bool Arbitro::terminoElJuego(){
+
+}
