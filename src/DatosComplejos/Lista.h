4cc1d7879b94431445644924c45a118974905f21
AndiFB <andi_fb96@hotmail.com>
2017-10-26 22:34:25 -0300

    lista agregada

--- src/DatosComplejos/Lista.h
+++ src/DatosComplejos/Lista.h
@@ -2,38 +2,70 @@
 #define LISTA_H_
 #endif
 #include "Nodo.h"
+#include <iostream>
 template <class T>
 class Lista
 {
 
   private:
-    Nodo<T> *primero;
+	Nodo<T>* primero;
 
     unsigned int tamanio;
 
-    Nodo<T> *cursor;
+   Nodo<T>* cursor;
 
 
     //Devuelve el nodo de la posicion buscada
-    obtenerNodo(int posicion);
+   	Nodo<T>* obtenerNodo(int posicion){
+   	    Nodo<T> *actual = this->primero;
+   	    for (int i = 1; i < posicion; i++) {
+   	        actual = actual->obtenerSiguiente();
+   	    }
+   	 std::cout << "nodo obtenido" << std::endl;
+
+   	    return actual;
+   	}
 
   public:
     /*
      * post: Lista vacía.
      */
-    Lista();
+    Lista(){
+        primero = NULL;
+        tamanio = 0;
+        cursor = NULL;
+        std::cout << "Lista creada" << std::endl;
+    }
 
     /*
      * post: deja el cursor de la Lista preparado para hacer un nuevo
      *       recorrido sobre sus elementos.
      */
-    void iniciarCursor();
+    void iniciarCursor(){
+        this->cursor = NULL;
+        std::cout << "Cursor iniciado" << std::endl;
+    }
 
     /*
      * pre : No se agregan ni se quitan elementos de la lista
      * post: Mueve al siguiente elemento, devuelve si hay proximo o no
      */
-    bool avanzarCursor();
+    bool avanzarCursor(){
+
+        if (cursor == NULL) {
+
+            cursor = primero;
+
+        } else {
+
+           cursor = cursor->obtenerSiguiente();
+        }
+
+        /* pudo avanzar si el cursor ahora apunta a un nodo */
+        std::cout << "Cursor avanzado" << std::endl;
+
+        return (cursor != NULL);
+    }
 
 
     /*
@@ -41,37 +73,97 @@ class Lista
      * post: devuelve el elemento en la posición del cursor.
      *
      */
-    T obtenerCursor();
+    T obtenerCursor(){
+
+    T elemento;
+
+    if (cursor != NULL) {
+
+        elemento = cursor->obtenerDato();
+    }
+    std::cout << "Cursor obtenido" << std::endl;
+
+    return elemento;
+}
 
     /*
      * post: indica si la Lista tiene algún elemento.
      */
-    bool estaVacia();
+    bool estaVacia(){
+    	std::cout << "Esta vacia?" << std::endl;
+        return (primero == NULL);
+    }
+
     /*
      * post: devuelve la cantidad de elementos que tiene la Lista.
      */
-    unsigned int contarElementos();
+    unsigned int contarElementos(){
+    	std::cout << "elementos contados" << std::endl;
+    	return tamanio;
+
+    }
     /*
      * post: agrega el elemento al final de la Lista, en la posición:
      *       contarElementos() + 1.
      */
-    void agregarElemento(T elemento);
+    void agregarElemento(T elemento){
+   		Nodo<T> *nuevo = new Nodo<T>(elemento);
+        int posicion = tamanio;
+    	if(estaVacia()){
+        	this->primero = nuevo;
+    	}else{
+        Nodo<T> *anterior = obtenerNodo(posicion);
+        anterior->cambiarSiguiente(nuevo);
+    	}
+        	tamanio ++;
+        std::cout << "Nuevo agregado" << std::endl;
+
+    }
 
     /*
      * pre : posición pertenece al intervalo: [1, contarElementos()]
      * post: devuelve el elemento en la posición indicada.
      */
-    T obtenerDato(unsigned int posicion);
+    T obtener(unsigned int posicion){
+    	std::cout << "Dato obtenido" << std::endl;
+
+        return obtenerNodo(posicion)->obtenerDato();
+    }
 
     /*
      * pre : posición pertenece al intervalo: [1, contarElementos()]
      * post: remueve de la Lista el elemento en la posición indicada.
      */
-    void removerNodo(unsigned int posicion);
+    void removerNodo(unsigned int posicion){
+        Nodo<T> *nodoARemover;
+        if(posicion == 1){
+            nodoARemover = primero;
+            primero->cambiarSiguiente(nodoARemover);
+        }
+        else{
+            Nodo<T> * anterior = obtenerNodo(posicion - 1);
+            nodoARemover = anterior->obtenerSiguiente();
+            anterior->cambiarSiguiente(nodoARemover->obtenerSiguiente());
+        }
+        delete nodoARemover;
+        tamanio --;
+        std::cout << "Nodo Removido" << std::endl;
+
+    }
       
      /*
       * post: libera los recursos asociados a la Lista.
       */
-    ~Lista();
+    ~Lista(){
 
-}
+        while (primero != NULL) {
+
+            Nodo<T>* aBorrar = primero;
+            primero = primero->obtenerSiguiente();
+            std::cout << "Nodo Borrado" << std::endl;
+
+            delete aBorrar;
+        }
+    }
+
+};
--- src/DatosComplejos/Nodo.h
+++ src/DatosComplejos/Nodo.h
@@ -1,9 +1,10 @@
 #ifndef NODO_H_
 #define NODO_H_
-
+#endif
 #ifndef NULL
 #define NULL 0
 #endif
+#include <iostream>
 template <class T> class Nodo{
 private:
     T dato;
@@ -14,23 +15,44 @@ public:
  * Pre: -
  * Post: Crea un nodo con la informacion dada, sin siguiente
  */
-    Nodo(T dato);
+    Nodo(T dato){
+    	this->dato = dato;
+       siguiente = NULL;
+       std::cout << "Nodo Creado" << std::endl;
+    }
 
 /*
 *Pre: -
 *Post: devuelve el dato del nodo
 */
-    T obtenerDato();
+    T obtenerDato(){
+        return dato;
+        std::cout << "Dato devuelto" << std::endl;
+
+    }
 /*
 *Post: cambia el dato del nodo
 */   
-    void cambiarDato(T datoNuevo);
+    void cambiarDato(T datoNuevo){
+        dato = datoNuevo;
+        std::cout << "Dato cambiado" << std::endl;
+
+    }
 /*
 *Post: Devuelve el siguiente del nodo actual
 */
-    Nodo<T> obtenerSiguiente();
+    Nodo<T>* obtenerSiguiente(){
+        return siguiente;
+        std::cout << "siguiente devuelto" << std::endl;
+
+    }
 /*
 *Post: Cambia el siguiente del nodo actual
 */
-    void cambiarSiguiente(Nodo<T>* siguienteNuevo);
+    void cambiarSiguiente(Nodo<T>* siguienteNuevo){
+        this->siguiente = siguienteNuevo;
+        std::cout << "Siguiente cambiado" << std::endl;
+
+    }
+
 };
