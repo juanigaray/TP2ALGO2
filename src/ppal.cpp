dde120409bd4b0694865e24b620c5339d321a960
Juan Garay <juangaray93@gmail.com>
2017-11-01 04:32:07 -0300

    Fin de interfaz dibuj-arbitr

--- src/ppal.cpp
+++ src/ppal.cpp
@@ -43,21 +43,21 @@ int main(){
 
 						int columnaADescubrir = arbitro.devolverColumnaDeJugada() - nColumna;
 			    		int filaADescubrir = arbitro.devolverFilaDeJugada() - nFila;
-			            if( (filaADescubrir > -1 ) && (columnaADescubrir > -1) &&
-			            		(filaADescubrir <= (int)arbitro.devolverFilaMaxima()) &&
-								columnaADescubrir <= (int)arbitro.devolverColumnaMaxima()){
+			            if( (filaADescubrir > 0 ) && (columnaADescubrir > 0) &&
+			            	(filaADescubrir <= (int)arbitro.devolverFilaMaxima() ) &&
+							columnaADescubrir <= (int)arbitro.devolverColumnaMaxima() ){
 
 			            	uint cantidadDeCircundantes = arbitro.evaluarBombasCircundantes(columnaADescubrir, filaADescubrir);
 							std::ostringstream ossNumeroADibujar;
 							ossNumeroADibujar << cantidadDeCircundantes;
 							std::string numeroADibujar = ossNumeroADibujar.str();
-							dibujante.cambiarCuadrante( columnaADescubrir, filaADescubrir, numeroADibujar, 0, false);
+							dibujante.cambiarCuadrante( columnaADescubrir -1, filaADescubrir -1, numeroADibujar, 0, false);
 			            }
 			        }
 				}
 			} else {
-				dibujante.cambiarCuadrante( arbitro.devolverColumnaDeJugada(),
-											arbitro.devolverFilaDeJugada(),
+				dibujante.cambiarCuadrante( arbitro.devolverColumnaDeJugada() -1,
+											arbitro.devolverFilaDeJugada() -1,
 											arbitro.devolverTipoDeJugada(),
 											arbitro.devolverTurno(),
 											false);
