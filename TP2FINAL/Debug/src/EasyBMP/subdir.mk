fd508d0a339348651d6cdd31381c55af0d4063d8
Juan <juangaray93@gmail.com>
2017-12-06 01:31:55 -0300

    tratando

--- TP2FINAL/Debug/src/EasyBMP/subdir.mk
+++ TP2FINAL/Debug/src/EasyBMP/subdir.mk
@@ -16,7 +16,7 @@ CPP_DEPS += \
 # Each subdirectory must supply rules for building sources it contributes
 src/EasyBMP/%.o: ../src/EasyBMP/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: Cross G++ Compiler'
+	@echo 'Invoking: GCC C++ Compiler'
 	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
--- TP2FINAL/Debug/src/subdir.mk
+++ TP2FINAL/Debug/src/subdir.mk
@@ -40,7 +40,7 @@ CPP_DEPS += \
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: Cross G++ Compiler'
+	@echo 'Invoking: GCC C++ Compiler'
 	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
